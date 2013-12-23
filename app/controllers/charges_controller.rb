38e846085e17a52fba91e69c601523f34393412a
Justin Tanacredi <Jtanacredi@gmail.com>
2013-12-22 21:57:13 -0500

    right before adding customer id

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -18,10 +18,19 @@ class ChargesController < ApplicationController
         :description => 'Rails Stripe customer',
         :currency    => 'usd'
     )
-
+    current_user.update_attribute(:customer_id, customer.id)
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
+  end
+
+  def charge
+    Stripe::Charge.create(
+        :amount   => @amount # or whatever the amount will be, discounted or not
+        :currency => "usd",
+        :customer => current_user.customer_id
+    # or if this is done without a current_user, you could loop over all the users who bought the product
+    )
   end
 
 end
--- app/models/sku.rb
+++ app/models/sku.rb
@@ -1,2 +1,3 @@
 class Sku < ActiveRecord::Base
+  belongs_to :product
 end
