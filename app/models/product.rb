45ddf250a62c8d7dfced0ca050adad3df3b9030f
Justin Tanacredi <Jtanacredi@gmail.com>
2014-01-23 22:20:43 -0500

    add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :skus
   has_many :orders, through: :skus
+  validates_presence_of :price, :title, :description, :image_url
   scope :active, -> { where(active: true) }
 
   def self.expired_and_active
