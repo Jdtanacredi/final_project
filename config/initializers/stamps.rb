620ddfbef26b2034a5ac521360769500377819ad
Justin Tanacredi <Jtanacredi@gmail.com>
2014-02-12 22:40:46 -0500

    Add button to stamps path

---
+++ config/initializers/stamps.rb
@@ -0,0 +1,5 @@
+Stamps.configure do |config|
+  #config.integration_id = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXX'
+  #config.username       = 'STAMPS USERNAME'
+  #config.password       = 'STAMPS PASSWORD'
+end
